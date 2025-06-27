3f027ef08d13a0d51a480969ea479ed21e9fd076
Minecube <126375609+Minecube1510@users.noreply.github.com>
2025-06-28 06:52:14 +0700

    Try Ngelink projects -  1

---
+++ syswork/js/link-pro.js
@@ -0,0 +1,17 @@
+/* link-pro.js */
+
+
+/* Linking */
+const alLink = [
+    ('compars-gosei-holoid/cgh-index.html'),  // [Comparation Gosei-HoloID]
+    ('cs/cs-index.html'),  // [ComingSoon]
+    ('cs/cs-index.html'),  // [ComingSoon]
+    ('cs/cs-index.html'),  // [ComingSoon]
+    ('cs/cs-index.html'),  // [ComingSoon]
+]
+/* Linking */
+
+
+/* Sistem */
+
+/* Sistem */
