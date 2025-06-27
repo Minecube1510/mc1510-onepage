8af9e38ad29273be6f44d97886a9a517a4899131
Minecube <126375609+Minecube1510@users.noreply.github.com>
2025-06-28 06:42:51 +0700

    Cenji Favic.js

--- syswork/js/favics.js
+++ syswork/js/favics.js
@@ -1,21 +1,27 @@
 /* favics.js */
 
-const faviconPath = '../asset/emoji-test.ico';
+// favics.js
+
+const iconIcoPath = '../asset/emoji-test.ico';
+const iconPngPath = '../asset/emoji-test.png';
+
 const commonAttributes = [
-  { rel: 'icon', type: 'image/x-icon', },
-  { rel: 'shortcut icon', type: 'image/x-icon', },
-  { rel: 'apple-touch-icon', type: 'image/x-icon', },
-  { rel: 'apple-touch-icon-precomposed', type: 'image/x-icon', },
-  { rel: 'apple-touch-icon', type: 'image/x-icon', sizes: '180x180', },
-  { rel: 'icon', type: 'image/png', }
+  { rel: 'icon', type: 'image/x-icon' },
+  { rel: 'shortcut icon', type: 'image/x-icon' },
+  { rel: 'apple-touch-icon', type: 'image/x-icon' },
+  { rel: 'apple-touch-icon-precomposed', type: 'image/x-icon' },
+  { rel: 'apple-touch-icon', type: 'image/x-icon', sizes: '180x180' },
+  { rel: 'icon', type: 'image/png' }
 ];
 
 commonAttributes.forEach(attrs => {
   const link = document.createElement('link');
-  link.href = faviconPath;
+  const isPng = attrs.type === 'image/png';
+  link.href = isPng ? iconPngPath : iconIcoPath;
+  
   Object.entries(attrs).forEach(([key, val]) => {
     link.setAttribute(key, val);
   });
+
   document.head.appendChild(link);
 });
-
