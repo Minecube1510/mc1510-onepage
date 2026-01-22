c42648d519eb38a3b357e6c675b07cd162838458
Minecube <Minekubus84s@gmail.com>
2026-01-22 20:38:45 +0700

    [Dev] Learning Web Basics - 2
    [Main] Merging Process - 1

---
+++ syswork/scripter.js
@@ -0,0 +1,29 @@
+/*
+ *
+ * Name: scripter.js
+ * syswork/scripter.js
+ * 
+ */
+
+/* Imports */
+//...
+
+
+/* Later */
+//
+
+
+/* Uji Coba */
+// Title
+let pageTitle = document.title;
+const baseTitle = (document.getElementById(("index-title").toLowerCase()));
+//
+baseTitle.textContent = ([
+    ("Judul Halaman"), (pageTitle)
+].join(" - "));
+//
+// Favicon
+//Later...
+
+
+/*|*/
