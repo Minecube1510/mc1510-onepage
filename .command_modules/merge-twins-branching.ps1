6b9ad56dd9b63960009a7af410907c30ee1070ae
Minecube <Minekubus84s@gmail.com>
2026-01-30 02:31:31 +0700

    [Bugfix] Adding Command Files - (1.3)

--- .command_modules/merge-twins-branching.ps1
+++ .command_modules/merge-twins-branching.ps1
@@ -1,56 +1,67 @@
-@echo off
-:: #
-:: {File-Name} "merge-twins-branching.ps1"
-:: {File-Path} ".command_modules/merge-twins-branching.ps1"
-:: {Formatted} ["ps1", ".ps1"]
-:: {Archived} ["cmd", ".cmd"], [""]
-:: |
-:: {Description Script}
-:: Merging Branch into Branch. Require two Branches.
-:: |
-:: {Usage}
-:: merge-twins-branching.cmd <BRANCH_MERGING> <BRANCH_MERGED>
+#@echo off#
+#-|-#
+# {File-Name} "merge-twins-branching.ps1"
+# {File-Path-1} ".command_modules/merge-twins-branching.ps1"
+# {File-Path-1} ".command_modules\merge-twins-branching.ps1"
+#~{Formatted}~#
+# ["ps1", ".ps1"]
+#~{Archived}~#
+# ["cmd", ".cmd"], [""]
+#-|-#
+#~{Description Script}~#
+# Merging Branch into Branch. Require two Branches.
+#-|-#
+#~{Usage}~#
+# merge-twins-branching.cmd <BRANCH_MERGING> <BRANCH_MERGED>
 
-:: {Prep} Set Branches
-set BRANCH_AS_ING=%~1
-set BRANCH_AS_EDD=%~2
-
-:: {Req-1} Required params
-if "%BRANCH_AS_ING%"=="" (
-    echo [ERROR] Required Branch as merging for other Branch!
-    echo Usage: %~nx0 <BRANCH_MERGING> <BRANCH_MERGED>
-    exit /b 1
-)
-if "%BRANCH_AS_EDD%"=="" (
-    echo [ERROR] Required Branch as that wanted to be merged!
-    echo Usage: %~nx0 <BRANCH_MERGING> <BRANCH_MERGED>
-    exit /b 1
-)
-::
-echo [ACTIVATING] Merging "%BRANCH_AS_ING%" into "%BRANCH_AS_EDD%".
-::
-:: {Req-2} Continue Reset (Y/N)
-set /p CONFIRM=Are you sure, to merging? (Y/N):
-::
-if /I "%CONFIRM%"=="Y" (
-    echo Executing...
-) else if /I "%CONFIRM%"=="N" (
-    echo Cancelled.
-    exit /b 0
-) else (
-    echo Invalid answering. Should be `Y/y`-OR-`N/n`.
-    exit /b 1
+# {Prep} Set Branches #
+param (
+    [string]$BRANCH_AS_ING,
+    [string]$BRANCH_AS_EDD
 )
 
-:: {Process} Merging both Branches
-git checkout %BRANCH_AS_ING%
-git pull origin %BRANCH_AS_ING%
-git checkout %BRANCH_AS_EDD%
-git merge %BRANCH_AS_ING%
-git push origin %BRANCH_AS_EDD%
-::
-:: {Ending} Done Reseting Branching
-echo Done Merge Process.
+# {Req-1} Required params #
+if (-not $BRANCH_AS_ING -or -not $BRANCH_AS_EDD) {
+    Write-Host ""
+    Write-Host "[ERROR] Missing Required Branches!"
+    Write-Host "Usage: merge-twins-branching.ps1 <BRANCH_MERGING> <BRANCH_MERGED>"
+    Write-Host ""
+    #
+    exit 1
+}
+#
+Write-Host "[ACTIVATING] Merging '$BRANCH_AS_ING' into '$BRANCH_AS_EDD'."
+#
+# {Req-2} Continue Reset (Yy/Nn) #
+$CONFIRM = Read-Host "Are you sure, to merging? (Yy/nN)"
+switch ($CONFIRM.ToUpper()) {
+    "Y" {
+        Write-Host "Executing..."
+    }
+    "N" {
+        Write-Host ""
+        Write-Host "Cancelled.";
+        Write-Host ""
+        #
+        exit 0
+    }
+    default {
+        Write-Host "Invalid answering. Should be (Y/y) or (N/n)."
+        Write-Host ""
+        #
+        exit 1
+    }
+}
+
+# {Process} Merging both Branches #
+git checkout $BRANCH_AS_ING
+git pull origin $BRANCH_AS_ING
+git checkout $BRANCH_AS_EDD
+git merge $BRANCH_AS_ING
+git push origin $BRANCH_AS_EDD
+#
+# {Ending} Done Reseting Branching #
+Write-Host "Done Merge Process."
 
 
-::-END-::
+#-END-#
--- .command_modules/reset-commit-branching.ps1
+++ .command_modules/reset-commit-branching.ps1
@@ -1,66 +1,78 @@
-@echo off
-:: #
-:: {File-Name} "reset-commit-branching.ps1"
-:: {File-Path} ".command_modules/reset-commit-branching.ps1"
-:: {Formatted} ["ps1", ".ps1"]
-:: {Archived} ["cmd", ".cmd"], [""]
-:: |
-:: {Description Script}
-:: Reset all Commits in Branch.
-:: |
-:: {Usage}
-:: reset-commit-branch.cmd <BRANCH_KINI> "<COMMIT_DESC>"
+#@echo off#
+#-|-#
+# {File-Name} "reset-commit-branching.ps1"
+# {File-Path-1} ".command_modules/reset-commit-branching.ps1"
+# {File-Path-2} ".command_modules\reset-commit-branching.ps1"
+#~{Formatted}~#
+# ["ps1", ".ps1"]
+#~{Archived}~#
+# ["cmd", ".cmd"], [""]
+#-|-#
+#~{Description Script}~#
+# Reset all Commits in Branch.
+#-|-#
+#~{Usage}~#
+# reset-commit-branch.cmd <BRANCH_KINI> "<COMMIT_DESC>"
 
-:: {Prep} Set Branch and Commit
-set BRANCH_KINI=%~1
-set COMMIT_DESC=%~2
-
-:: {Req-1} Required params
-if "%BRANCH_KINI%"=="" (
-    echo [ERROR-DETECT] Required Branch Name!
-    echo Usage: %~nx0 <BRANCH_KINI> "<COMMIT_DESC>"
-    exit /b 1
-)
-if "%COMMIT_DESC%"=="" (
-    echo [ERROR-DETECT] Required Commit Description!
-    echo Usage: %~nx0 <BRANCH_KINI> "<COMMIT_DESC>"
-    exit /b 1
-)
-::
-echo Targets branch : %BRANCH_KINI%
-echo Commit message : %COMMIT_DESC%
-::
-:: {Req-2} Continue Reset (Y/N)
-set /p CONFIRM=Are you sure, to reset branch "%BRANCH_KINI%"? (Y/N): 
-::
-if /I "%CONFIRM%"=="Y" (
-    echo Executing...
-) else if /I "%CONFIRM%"=="N" (
-    echo Cancelled.
-    exit /b 0
-) else (
-    echo Invalid answering. Should be `Y/y`-OR-`N/n`.
-    exit /b 1
+# {Prep} Set Branch and Commit #
+param (
+    [string]$BRANCH_KINI,
+    [string]$COMMIT_DESC
 )
 
-:: {Process} Reseting Branching
-::git branch backup-%BRANCH_KINI% %BRANCH_KINI%
+# {Req-1} Required params #
+if (-not $BRANCH_KINI -or -not $COMMIT_DESC) {
+    Write-Host ""
+    Write-Host "[ERROR-DETECT] Required Branch and Commit!"
+    Write-Host "Usage: reset-commit-branching.ps1 <BRANCH_KINI> '<COMMIT_DESC>'"
+    Write-Host ""
+    #
+    exit 1
+}
+#
+Write-Host "Targets branch : $BRANCH_KINI"
+Write-Host "Commit message : $COMMIT_DESC"
+#
+# {Req-2} Continue Reset (Yy/Nn) #
+$CONFIRM = Read-Host "Are you sure, to reset branch '$BRANCH_KINI'? (Yy/Nn)"
+#
+switch ($CONFIRM.ToUpper()) {
+    "Y" {
+        Write-Host "Executing..."
+    }
+    "N" {
+        Write-Host ""
+        Write-Host "Cancelled.";
+        Write-Host ""
+        #
+        exit 0
+    }
+    default {
+        Write-Host "Invalid answering. Should be (Y/y) or (N/n)."
+        Write-Host ""
+        #
+        exit 1
+    }
+}
 
+# {Process} Reseting Branching #
+#git branch backup-%BRANCH_KINI% %BRANCH_KINI%
+#
 git checkout --orphan BRANCH_TUMBAL
 git add -A
-git commit -m "%COMMIT_DESC%"
-git branch -D %BRANCH_KINI%
-git branch -m %BRANCH_KINI%
+git commit -m "$COMMIT_DESC"
+git branch -D $BRANCH_KINI
+git branch -m $BRANCH_KINI
 git checkout --orphan BRANCH_BARU
 git rm -rf .
 git add .
-git commit -m "%COMMIT_DESC%"
-git branch -f %BRANCH_KINI%
-git checkout %BRANCH_KINI%
-git push origin %BRANCH_KINI% --force
-::
-:: {Ending} Done Reseting Branching
-echo Done for Reset Branch, %BRANCH_KINI%.
+git commit -m "$COMMIT_DESC"
+git branch -f $BRANCH_KINI
+git checkout $BRANCH_KINI
+git push origin $BRANCH_KINI --force
+#
+# {Ending} Done Reseting Branching #
+Write-Host "Done for Reset Branch, $BRANCH_KINI."
 
 
-::-END-::
+#-END-#
