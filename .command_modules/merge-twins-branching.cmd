96ac30ea5459c393e2821b10d592306223842f83
Minecube <Minekubus84s@gmail.com>
2026-01-30 01:59:15 +0700

    [Bugfix] Adding Command Files - 1

---
+++ .command_modules/merge-twins-branching.cmd
@@ -0,0 +1,55 @@
+@echo off
+:: #
+:: {File-Name} "merge-twins-branching.cmd"
+:: {File-Path} ".command_modules/merge-twins-branching.cmd"
+:: {Formatted} "cmd", ".cmd"
+:: |
+:: {Description Script}
+:: Merging Branch into Branch. Require two Branches.
+:: |
+:: {Usage}
+:: merge-twins-branching.cmd <BRANCH_MERGING> <BRANCH_MERGED>
+
+:: {Prep} Set Branches
+set BRANCH_AS_ING=%~1
+set BRANCH_AS_EDD=%~2
+
+:: {Req-1} Required params
+if "%BRANCH_AS_ING%"=="" (
+    echo [ERROR] Required Branch as merging for other Branch!
+    echo Usage: %~nx0 <BRANCH_MERGING> <BRANCH_MERGED>
+    exit /b 1
+)
+if "%BRANCH_AS_EDD%"=="" (
+    echo [ERROR] Required Branch as that wanted to be merged!
+    echo Usage: %~nx0 <BRANCH_MERGING> <BRANCH_MERGED>
+    exit /b 1
+)
+::
+echo [ACTIVATING] Merging "%BRANCH_AS_ING%" into "%BRANCH_AS_EDD%".
+::
+:: {Req-2} Continue Reset (Y/N)
+set /p CONFIRM=Are you sure, to merging? (Y/N):
+::
+if /I "%CONFIRM%"=="Y" (
+    echo Executing...
+) else if /I "%CONFIRM%"=="N" (
+    echo Cancelled.
+    exit /b 0
+) else (
+    echo Invalid answering. Should be `Y/y`-OR-`N/n`.
+    exit /b 1
+)
+
+:: {Process} Merging both Branches
+git checkout %BRANCH_AS_ING%
+git pull origin %BRANCH_AS_ING%
+git checkout %BRANCH_AS_EDD%
+git merge %BRANCH_AS_ING%
+git push origin %BRANCH_AS_EDD%
+::
+:: {Ending} Done Reseting Branching
+echo Done Merge Process.
+
+
+::-END-::
---
+++ .command_modules/reset-commit-branching.cmd
@@ -0,0 +1,65 @@
+@echo off
+:: #
+:: {File-Name} "reset-commit-branching.cmd"
+:: {File-Path} ".command_modules/reset-commit-branching.cmd"
+:: {Formatted} "cmd", ".cmd"
+:: |
+:: {Description Script}
+:: Reset all Commits in Branch.
+:: |
+:: {Usage}
+:: reset-commit-branch.cmd <BRANCH_KINI> "<COMMIT_DESC>"
+
+:: {Prep} Set Branch and Commit
+set BRANCH_KINI=%~1
+set COMMIT_DESC=%~2
+
+:: {Req-1} Required params
+if "%BRANCH_KINI%"=="" (
+    echo [ERROR-DETECT] Required Branch Name!
+    echo Usage: %~nx0 <BRANCH_KINI> "<COMMIT_DESC>"
+    exit /b 1
+)
+if "%COMMIT_DESC%"=="" (
+    echo [ERROR-DETECT] Required Commit Description!
+    echo Usage: %~nx0 <BRANCH_KINI> "<COMMIT_DESC>"
+    exit /b 1
+)
+::
+echo Targets branch : %BRANCH_KINI%
+echo Commit message : %COMMIT_DESC%
+::
+:: {Req-2} Continue Reset (Y/N)
+set /p CONFIRM=Are you sure, to reset branch "%BRANCH_KINI%"? (Y/N): 
+::
+if /I "%CONFIRM%"=="Y" (
+    echo Executing...
+) else if /I "%CONFIRM%"=="N" (
+    echo Cancelled.
+    exit /b 0
+) else (
+    echo Invalid answering. Should be `Y/y`-OR-`N/n`.
+    exit /b 1
+)
+
+:: {Process} Reseting Branching
+::git branch backup-%BRANCH_KINI% %BRANCH_KINI%
+
+git checkout --orphan BRANCH_TUMBAL
+git add -A
+git commit -m "%COMMIT_DESC%"
+git branch -D %BRANCH_KINI%
+git branch -m %BRANCH_KINI%
+git checkout --orphan BRANCH_BARU
+git rm -rf .
+git add .
+git commit -m "%COMMIT_DESC%"
+git branch -f %BRANCH_KINI%
+git checkout %BRANCH_KINI%
+git push origin %BRANCH_KINI% --force
+::
+:: {Ending} Done Reseting Branching
+echo Done for Reset Branch, %BRANCH_KINI%.
+
+
+::-END-::
