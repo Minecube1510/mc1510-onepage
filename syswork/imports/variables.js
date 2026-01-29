e3f763d30eb5dbe9ffb476e371a8e4301957098a
Minecube <Minekubus84s@gmail.com>
2026-01-29 22:26:01 +0700

    [Main.Clean] Rebase Project Struct - (1.0)

---
+++ syswork/imports/variables.js
@@ -0,0 +1,48 @@
+/*
+ *
+ * Name: "variables.js"
+ * Link: "syswork/imports/variables.js"
+ *
+ */
+//* * *//
+
+
+/* Variable - Characters */
+export const varOne = {
+    Blank: (""), Empty: (""),
+    Space: (" "),
+    //
+    Dash: ("/"),
+    Pipe: ("|"),
+    Uscore: ("_"),
+    HashT: ("#"),
+    //
+    Dot: ("."), Spot: ("."),
+    Coma: (","), Comma: (","),
+    Excla: ("!"),
+    Quest: ("?"),
+    AtSign: ("@"),
+    //
+    // "[" "]"
+    // "(" ")"
+    // "{" "}"
+    //
+    TwinColon: (":"),
+    SemiColon: (";"),
+    //
+    SingleQuo: (`'`),
+    DoubleQuo: (`"`),
+    BackTicko: ('`'),
+    //
+    Plus: ("+"),
+    Minus: ("-"),
+};
+//* * *//
+//
+
+
+/* Variable - Later... */
+//
+
+
+// !-! //
