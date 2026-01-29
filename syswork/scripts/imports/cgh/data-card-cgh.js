9f4f287a1a9d5ad92acb791c9d7d23bfa973ec28
Minecube <Minekubus84s@gmail.com>
2026-01-30 02:52:35 +0700

    [Bugfix] Defaulted Files for Live Serving - 1

--- syswork/scripts/imports/cgh/data-card-cgh.js
+++ syswork/scripts/imports/cgh/data-card-cgh.js
@@ -8,11 +8,11 @@
 
 /* Imports */
 import * as jsVars from
-    // "/mc1510-onepage/syswork/imports/variables.js";
-    "/syswork/imports/variables.js";
+    "/mc1510-onepage/syswork/imports/variables.js";
+    // "/syswork/imports/variables.js";
 import * as jsFuncs from
-    // "/mc1510-onepage/syswork/imports/functings.js";
-    "/syswork/imports/functings.js";
+    "/mc1510-onepage/syswork/imports/functings.js";
+    // "/syswork/imports/functings.js";
 //* * *//
 //
 
